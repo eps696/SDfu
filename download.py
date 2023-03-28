f9fe30d9c7ef5561498072fc22e1db78c4d23b4f
vadim epstein <eps@here.ru>
2023-03-28 23:39:41 +0300

    download fix

--- download.py
+++ download.py
@@ -26,23 +26,26 @@ def get_model(url: str, root: str = "./models", unzip=True):
             zf.extractall(root)
         os.remove(download_target)
 
+def main():
+    print(' downloading complete SD 1.5 model')
+    get_model("https://www.dropbox.com/s/9wuhum8w0iqs4o7/sdfu-v15-full-fp16.zip?dl=1", 'models/v1')
+    print(' downloading SD 1.5-inpainting Unet')
+    get_model("https://www.dropbox.com/s/bzmjnslk2sgsbt9/sdfu-v15i-unet-fp16.zip?dl=1", 'models/v1')
+    print(' downloading complete SD 2.1 model')
+    get_model("https://www.dropbox.com/s/38876tjuklvwq82/sdfu-v21-full-fp16.zip?dl=1", 'models/v2')
+    print(' downloading SD 2-inpainting model Unet')
+    get_model("https://www.dropbox.com/s/r5fa1mdxpw9e8k2/sdfu-v2i-unet-fp16.zip?dl=1", 'models/v2')
+    print(' downloading SD 2-depth model Unet & depth')
+    get_model("https://www.dropbox.com/s/4visx4qcscirvob/sdfu-v2d-unet-fp16.zip?dl=1", 'models/v2')
+    print(' downloading complete SD 2.1-v model')
+    get_model("https://www.dropbox.com/s/10gbecrugca1ydv/sdfu-v21v-full-fp16.zip?dl=1", 'models/v2v')
 
-print(' downloading complete SD 1.5 model')
-get_model("https://www.dropbox.com/s/9wuhum8w0iqs4o7/sdfu-v15-full-fp16.zip?dl=1", 'models/v1')
-print(' downloading SD 1.5-inpainting Unet')
-get_model("https://www.dropbox.com/s/bzmjnslk2sgsbt9/sdfu-v15i-unet-fp16.zip?dl=1", 'models/v1')
-print(' downloading complete SD 2.1 model')
-get_model("https://www.dropbox.com/s/38876tjuklvwq82/sdfu-v21-full-fp16.zip?dl=1", 'models/v2')
-print(' downloading SD 2-inpainting model Unet')
-get_model("https://www.dropbox.com/s/r5fa1mdxpw9e8k2/sdfu-v2i-unet-fp16.zip?dl=1", 'models/v2')
-print(' downloading SD 2-depth model Unet & depth')
-get_model("https://www.dropbox.com/s/4visx4qcscirvob/sdfu-v2d-unet-fp16.zip?dl=1", 'models/v2')
-print(' downloading complete SD 2.1-v model')
-get_model("https://www.dropbox.com/s/10gbecrugca1ydv/sdfu-v21v-full-fp16.zip?dl=1", 'models/v2v')
+    print(' downloading SD 1.x extra VAE models')
+    get_model("https://www.dropbox.com/s/z9uycihl6tybx9y/sdfu-v1-vaes-fp16.zip?dl=1", 'models/v1')
 
-print(' downloading SD 1.x extra VAE models')
-get_model("https://www.dropbox.com/s/z9uycihl6tybx9y/sdfu-v1-vaes-fp16.zip?dl=1", 'models/v1')
+    print(' downloading CLIPseg model')
+    get_model("https://www.dropbox.com/s/c0tduhr4g0al1cq/rd64-uni.pth?dl=1", 'models/clipseg', unzip=False)
 
-print(' downloading CLIPseg model')
-get_model("https://www.dropbox.com/s/c0tduhr4g0al1cq/rd64-uni.pth?dl=1", 'models/clipseg', unzip=False)
 
+if __name__ == '__main__':
+    main()
