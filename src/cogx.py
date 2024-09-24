3976c335292a5a1b59003907d78af2f0fe20d2ad
vadim epstein <eps@here.ru>
2024-09-25 00:57:39 +0300

    cogx fix

--- src/cogx.py
+++ src/cogx.py
@@ -2,6 +2,7 @@
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
 import numpy as np
+from PIL import Image
 import imageio
 import inspect
 import math
@@ -21,7 +22,6 @@ from diffusers import CogVideoXPipeline
 from diffusers import AutoencoderKLCogVideoX, CogVideoXTransformer3DModel
 from diffusers.schedulers import CogVideoXDDIMScheduler, CogVideoXDPMScheduler
 from diffusers.video_processor import VideoProcessor
-from diffusers.models.embeddings import get_3d_rotary_pos_embed
 from diffusers.utils import load_image, load_video
 
 from core.args import main_args
@@ -94,15 +94,6 @@ class CogX:
             self.sched_kwargs['generator'] = self.g_
 
 
-    def get_rot_pos_emb(self, height, width, num_frm=13):
-        gridH = height // (self.vae_scale_s * self.transformer.config.patch_size)
-        gridW = width  // (self.vae_scale_s * self.transformer.config.patch_size)
-        baseW =    720 // (self.vae_scale_s * self.transformer.config.patch_size)
-        baseH =    480 // (self.vae_scale_s * self.transformer.config.patch_size)
-        gridcrop_coords = get_resize_crop_region_for_grid((gridH, gridW), baseW, baseH)
-        freqs_cos, freqs_sin = get_3d_rotary_pos_embed(self.transformer.config.attention_head_dim, gridcrop_coords, (gridH, gridW), num_frm, use_real=True)
-        return freqs_cos.to(self.device), freqs_sin.to(self.device)
-
     def set_steps(self, steps=None, sigmas=None, strength=1.):
         if sigmas is None:
             self.scheduler.set_timesteps(steps, device=self.device)
@@ -131,6 +122,13 @@ class CogX:
             cs = torch.cat([uc, cs])
         return cs
 
+    def lat_vid(self, lat):
+        lat = lat.permute(0,2,1,3,4) # [b,c,f,h,w]
+        video = self.vae.decode(lat / self.vae.config.scaling_factor).sample
+        video = (video / 2 + 0.5).clamp(0,1)
+        video = video.permute(0,2,1,3,4) # [b,f,c,h,w]
+        return video
+
     def get_vaelat(self, enc_out, sample_mode="sample"):
         if hasattr(enc_out, "latent_dist") and sample_mode == "sample":
             return enc_out.latent_dist.sample(self.g_)
@@ -144,47 +142,42 @@ class CogX:
     def init_lat(self, src, dtype):
         lat = [self.get_vaelat(self.vae.encode(x.unsqueeze(0))) for x in src]
         lat = torch.cat(lat, dim=0).to(dtype).permute(0,2,1,3,4)
-        lat = self.vae.config.scaling_factor * lat
-        return lat # [b,f,c,h,w]
+        return lat * self.vae.config.scaling_factor # [b,f,c,h,w]
 
     @staticmethod
-    def cals_ids(length, count):
+    def calc_ids(length, count):
         if count == 1: return [0]
         return [int(i * (length-1) / (count-1)) for i in range(count)]
 
     def make_lat(self, bs=1, ch=16, num_frames=49, height=60, width=90, image=None, imgpos=None, video=None, dtype=None):
         num_frm = (num_frames - 1) // self.vae_scale_t + 1
         shape = (bs, num_frm, ch, height // self.vae_scale_s, width // self.vae_scale_s) # [1,13,16,60,90]
-
         img_lat = None
         if image is not None: # [n,c,h,w]
-            if imgpos is None: imgpos = self.cals_ids(num_frm, len(image))
+            if imgpos is None: imgpos = self.calc_ids(num_frm, len(image))
             img_lat = torch.zeros(shape, device=self.device, dtype=dtype) # [1,13,16,60,90]
             in_lats = self.init_lat(image.unsqueeze(2), dtype) # [n,1,16,60,90]
             for i, n in enumerate(imgpos):
                 img_lat[:,n] = in_lats[i]
-
         elif video is not None and num_frames > video.size(2): # video expansion
             img_lat = self.init_lat(video, dtype) # [b,f,c,h,w]
             pad_shape = (bs, num_frm - img_lat.shape[1], *shape[2:])
             pad_lat = torch.zeros(pad_shape, device=self.device, dtype=dtype)
             img_lat = torch.cat([img_lat, pad_lat], dim=1) # [b,F,c,h,w]
-        
         lat = torch.randn(shape, generator=self.g_, device=self.device, dtype=dtype)
         if video is not None: # [b,c,f,h,w]
             vid_lat = self.init_lat(video, dtype) # [b,f,c,h,w]
             vid_frm = vid_lat.shape[1]
             lat[:,:vid_frm] = self.scheduler.add_noise(vid_lat, lat[:,:vid_frm], self.lat_timestep)
         lat *= self.scheduler.init_noise_sigma
-
         return lat, img_lat
 
-    def lat_vid(self, lat):
-        lat = lat.permute(0,2,1,3,4) # [b,c,f,h,w]
-        video = self.vae.decode(lat / self.vae.config.scaling_factor).sample
-        video = (video / 2 + 0.5).clamp(0,1)
-        video = video.permute(0,2,1,3,4) # [b,f,c,h,w]
-        return video
+    def get_rot_pos_emb(self, H, W, num_frm=13, time_ids=None):
+        gridH, gridW, baseH, baseW = [x // (self.vae_scale_s * self.transformer.config.patch_size) for x in [H, W, 480, 720]]
+        gridcrop_coords = get_resize_crop_region_for_grid(gridH, gridW, baseH, baseW)
+        if time_ids is None: time_ids = torch.arange(num_frm, dtype=torch.float32)
+        freqs_cos, freqs_sin = get_3d_rot_pos_emb(self.transformer.config.attention_head_dim, gridcrop_coords, gridH, gridW, time_ids)
+        return freqs_cos.to(self.device), freqs_sin.to(self.device)
 
 
     def generate(self, cs, H, W, num_frames, image=None, imgpos=None, video=None):
@@ -197,8 +190,8 @@ class CogX:
         fcount = lat.size(1)
         img_rot_emb = self.get_rot_pos_emb(H, W, self.a.ctx_frames) if self.transformer.config.use_rotary_positional_embeddings else None
 
-        def calc_noise(x, t, conds):
-            noise_pred = self.transformer(x, conds, t.expand(x.shape[0]), image_rotary_emb=img_rot_emb, return_dict=False)[0]
+        def calc_noise(x, t, conds, img_rot_emb):
+            noise_pred = self.transformer(x, conds, t.expand(x.shape[0]), image_rotary_emb=img_rot_emb).sample
             if len(x) > 1 and len(conds) > 1:
                 cfg_scale = 1 + self.a.cfg_scale * ((1 - math.cos(math.pi * ((steps - t.item()) / steps) ** 5.)) / 2) if self.a.dyn_cfg else self.a.cfg_scale
                 noise_un, noise_c = noise_pred.chunk(bs)
@@ -218,12 +211,13 @@ class CogX:
                 noise_pred = torch.zeros_like(lat)
                 slide_count = torch.zeros((1, fcount, 1, 1, 1), device=lat_in.device)
                 for slids in uniform_slide(i, fcount, self.a.ctx_frames, loop=self.a.loop):
-                    noise_pred_sub = calc_noise(lat_in[:,slids], t, cs)
+                    if img_rot_emb is not None: img_rot_emb = self.get_rot_pos_emb(H, W, time_ids = slids)
+                    noise_pred_sub = calc_noise(lat_in[:,slids], t, cs, img_rot_emb)
                     noise_pred[:,slids] += noise_pred_sub
                     slide_count[:,slids] += 1 # increment which indices were used
                 noise_pred /= slide_count
             else: # single context video
-                noise_pred = calc_noise(lat_in, t, cs)
+                noise_pred = calc_noise(lat_in, t, cs, img_rot_emb)
 
             if isinstance(self.scheduler, CogVideoXDPMScheduler):
                 lat, old_pred = self.scheduler.step(noise_pred, old_pred, t, self.timesteps[i-1] if i > 0 else None, lat, **self.sched_kwargs, return_dict=False)
@@ -235,50 +229,82 @@ class CogX:
         video = self.lat_vid(lat) # [b,f,c,h,w]
         return video
 
-# sliding sampling for long videos
-# from https://github.com/ArtVentureX/comfyui-animatediff/blob/main/animatediff/sliding_schedule.py
-def ordered_halving(val, verbose=False): # Returns fraction that has denominator that is a power of 2
-    bin_str = f"{val:064b}" # get binary value, padded with 0s for 64 bits
-    bin_flip = bin_str[::-1] # flip binary value, padding included
-    as_int = int(bin_flip, 2) # convert binary to int
-    final = as_int / (1 << 64) # divide by 1 << 64, equivalent to 2**64, or 18446744073709551616, or 1 with 64 zero's
-    if verbose: print(f"$$$$ final: {final}")
-    return final
-# generate lists of latent indices to process
-def uniform_slide(step, num_frames, ctx_size=16, ctx_stride=1, ctx_overlap=5, loop=True, verbose=False):
-    if num_frames <= ctx_size:
-        yield list(range(num_frames))
-        return
-    ctx_stride = min(ctx_stride, int(np.ceil(np.log2(num_frames / ctx_size))) + 1)
-    pad = int(round(num_frames * ordered_halving(step, verbose)))
-    fstop = num_frames + pad + (0 if loop else -ctx_overlap)
-    for ctx_step in 1 << np.arange(ctx_stride):
-        fstart = int(ordered_halving(step) * ctx_step) + pad
-        fstep = ctx_size * ctx_step - ctx_overlap
-        for j in range(fstart, fstop, fstep):
-            yield [e % num_frames for e in range(j, j + ctx_size * ctx_step, ctx_step)]
+# slightly edited diffusers.models.embeddings.get_3d_rotary_pos_embed
+def get_3d_rot_pos_emb(embed_dim, crops_coords, h, w, time_ids, theta=10000, use_real=True): # RoPE for video tokens with 3D structure.
+    # embed_dim = hidden_size_head, theta = Scaling factor for frequency computation
 
-def save_video(video, path, fps):
-    if torch.is_tensor(video):
-        video = (video * 255).round().clamp(0,255).permute(0,2,3,1).to(torch.uint8).cpu().numpy() # [f,h,w,c]
-    with imageio.get_writer(path, fps=fps) as writer:
-        for frame in video:
-            writer.append_data(frame)
+    def make_freq(grid, dim):
+        freqs = 1.0 / (theta ** (torch.arange(0, dim, 2).float() / dim))
+        return torch.einsum("n , f -> n f", grid, freqs).repeat_interleave(2, dim=-1)
 
-def get_resize_crop_region_for_grid(src, tgt_width, tgt_height):
-    tw = tgt_width
-    th = tgt_height
-    h, w = src
-    r = h / w
-    if r > (th / tw):
+    (h0, w0), (h1, w1) = crops_coords # top-left and bottom-right coordinates of the crop
+    grid_h = torch.arange(h0, h1, step = (h1-h0)/h, dtype=torch.float32)
+    grid_w = torch.arange(w0, w1, step = (w1-w0)/w, dtype=torch.float32)
+    grid_t = torch.tensor(time_ids).float()
+    freqs_t = make_freq(grid_t, embed_dim // 4)
+    freqs_h = make_freq(grid_h, embed_dim // 8 * 3)
+    freqs_w = make_freq(grid_w, embed_dim // 8 * 3)
+
+    def broadcast(tensors, dim=-1): # Broadcast and concatenate tensors along specified dimension
+        num_tensors = len(tensors)
+        shape_lens = {len(t.shape) for t in tensors}
+        assert len(shape_lens) == 1, "tensors must all have the same number of dimensions"
+        shape_len = list(shape_lens)[0]
+        dim = (dim + shape_len) if dim < 0 else dim
+        dims = list(zip(*(list(t.shape) for t in tensors)))
+        expandable_dims = [(i, val) for i, val in enumerate(dims) if i != dim]
+        assert all([*(len(set(t[1])) <= 2 for t in expandable_dims)]), "invalid dimensions for broadcastable concatenation"
+        max_dims = [(t[0], max(t[1])) for t in expandable_dims]
+        expanded_dims = [(t[0], (t[1],) * num_tensors) for t in max_dims]
+        expanded_dims.insert(dim, (dim, dims[dim]))
+        expandable_shapes = list(zip(*(t[1] for t in expanded_dims)))
+        tensors = [t[0].expand(*t[1]) for t in zip(tensors, expandable_shapes)]
+        return torch.cat(tensors, dim=dim)
+
+    freqs = broadcast((freqs_t[:, None, None, :], freqs_h[None, :, None, :], freqs_w[None, None, :, :]), dim=-1) # [13,30,45,64] t h w d
+    t, h, w, d = freqs.shape
+    freqs = freqs.view(t * h * w, d)
+    sin = freqs.sin()
+    cos = freqs.cos()
+    if use_real: return cos, sin # real and imaginary parts separately, [len(time_ids) * h * w, embed_dim/2]
+    else:        return torch.polar(torch.ones_like(freqs), freqs) # complex numbers
+
+def get_resize_crop_region_for_grid(h, w, th, tw):
+    if h/w > th/tw:
         resize_height = th
         resize_width = int(round(th / h * w))
     else:
         resize_width = tw
         resize_height = int(round(tw / w * h))
-    crop_top = int(round((th - resize_height) / 2.0))
-    crop_left = int(round((tw - resize_width) / 2.0))
+    crop_top = int(round((th - resize_height) / 2.))
+    crop_left = int(round((tw - resize_width) / 2.))
     return (crop_top, crop_left), (crop_top + resize_height, crop_left + resize_width)
+
+# sliding sampling for long videos
+# from https://github.com/ArtVentureX/comfyui-animatediff/blob/main/animatediff/sliding_schedule.py
+def uniform_slide(step, num_frames, size=13, overlap=0, loop=True): # stride = 1
+    pad = int(round(num_frames * ordered_halving(step)))
+    fstop = num_frames + pad + (0 if loop else -overlap)
+    fstart = int(ordered_halving(step)) + pad
+    fstep = size - overlap
+    for j in range(fstart, fstop, fstep):
+        yield [e % num_frames for e in range(j, j + size)]
+def ordered_halving(val): # Returns fraction that has denominator that is a power of 2
+    bin_str = f"{val:064b}" # get binary value, padded with 0s for 64 bits
+    bin_flip = bin_str[::-1] # flip binary value, padding included
+    as_int = int(bin_flip, 2) # convert binary to int
+    final = as_int / (1 << 64) # divide by 1 << 64, equivalent to 2**64, or 18446744073709551616, or 1 with 64 zero's
+    return final
+
+def save_video(video, path, fps=12, ext='jpg'):
+    if torch.is_tensor(video):
+        video = (video * 255).round().clamp(0,255).permute(0,2,3,1).to(torch.uint8).cpu().numpy() # [f,h,w,c]
+    with imageio.get_writer(path + '.mp4', fps=fps) as writer:
+        for frame in video:
+            writer.append_data(frame)
+    os.makedirs(path, exist_ok=True)
+    for i, frame in enumerate(video):
+        Image.fromarray(frame).save(os.path.join(path, '%05d.%s' % (i, ext)), quality=95, subsampling=0)
 
 
 @torch.no_grad()
@@ -311,7 +337,7 @@ def main():
 
     W, H = [720,480] if a.size is None else [int(s) for s in a.size.split('-')]
     # [x * cogx.vae_scale_s for x in [cogx.transformer.config.sample_width, cogx.transformer.config.sample_height]]
-    a.in_txt = ['. '.join([a.pretxt, s, a.postxt]) for s in read_txt(a.in_txt)]
+    a.in_txt = [''.join([a.pretxt, s, a.postxt]) for s in read_txt(a.in_txt)]
     if a.verbose: save_cfg(a, a.out_dir)
     count = 1
 
@@ -339,8 +365,8 @@ def main():
     if isset(a, 'in_img'): outname += [basename(a.in_img)]
     if len(a.in_txt) > 0: outname += [txt_clean(basename(a.in_txt[0] if isinstance(a.in_txt, list) else a.in_txt))]
     outname = '-'.join(outname)[:69]
-    outpath = os.path.join(a.out_dir, '%s-%d.mp4' % (outname, cogx.seed))
-    save_video(video, outpath, a.fps)
+    outname = os.path.join(a.out_dir, '%s-%d' % (outname, cogx.seed))
+    save_video(video, outname)
 
 
 if __name__ == '__main__':
